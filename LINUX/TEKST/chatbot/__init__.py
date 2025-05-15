cc9f64af1ad4e243388412bf465dc8774d2ee0b6
Ayush Chauhan <132136288+eyesee11@users.noreply.github.com>
2025-05-15 12:40:49 +0530

    Delete LINUX/TEKST directory

--- LINUX/TEKST/chatbot/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Chatbot package for the Text Editor Launcher application.
-This package contains modules for AI assistant functionality.
-"""
-
-from .chatbot import Chatbot
-
-__all__ = ['Chatbot']
